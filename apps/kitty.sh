e9be019376644a923fb44239d1bd2e32b23e03eb
Coen de Wit <coen@tcr-it.nl>
2025-08-13 22:51:21 +0200

    Add kitty

--- apps/kitty.sh
+++ apps/kitty.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "=== Install and configure Kitty ==="
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 source "$SCRIPT_DIR/../helper_functions.sh"
 
 install_if_not_found kitty
