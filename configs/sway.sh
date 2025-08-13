029cec221b4b131d565bb7c37ab069e0bf36284e
Coen de Wit <coen@tcr-it.nl>
2025-08-14 00:07:11 +0200

    Create symlinks in stead of copies

--- configs/sway.sh
+++ configs/sway.sh
@@ -5,6 +5,9 @@ echo "=== Sway overrides ==="
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
-mkdir -p ~/.config/sway
+source "$SCRIPT_DIR/../helper_functions.sh"
 
-cp -r "$SCRIPT_DIR/sway/" ~/.config/sway/definitions.d/
+create_symlink "$SCRIPT_DIR/sway/bindings.conf" "$HOME/.config/sway/config.d/bindings.conf"
+create_symlink "$SCRIPT_DIR/sway/keyboard.conf" "$HOME/.config/sway/config.d/keyboard.conf"
+create_symlink "$SCRIPT_DIR/sway/monitor.conf" "$HOME/.config/sway/config.d/monitor.conf"
+create_symlink "$SCRIPT_DIR/sway/terminal.conf" "$HOME/.config/sway/config.d/terminal.conf"
