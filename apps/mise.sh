6767f5b41329f36bcf459191912f2186c5badb40
Coen de Wit <coen@tcr-it.nl>
2025-08-14 00:04:25 +0200

    Include messages if installed

--- apps/mise.sh
+++ apps/mise.sh
@@ -3,14 +3,20 @@ echo "=== Mise installation ==="
 if ! command -v mise &>/dev/null; then
   curl https://get.mise.run | sh
   echo "eval \"\$(~/.local/bin/mise activate zsh)\"" >>~/.zshrc
+else
+  echo "Mise is already installed"
 fi
 
 echo "=== .Net installation ==="
 if ! command -v dotnet &>/dev/null; then
   mise install dotnet
+else
+  echo "Dotnet is already installed"
 fi
 
 echo "=== Golang installation ==="
 if ! command -v go &>/dev/null; then
   mise install go
+else
+  echo "Golang is already installed"
 fi
