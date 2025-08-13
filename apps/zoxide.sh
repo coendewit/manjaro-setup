a3cb43c07daa694756b717d6dddc769216b18f6a
Coen de Wit <coen@tcr-it.nl>
2025-08-13 22:49:45 +0200

    Make zoxide installation idempotent

--- apps/zoxide.sh
+++ apps/zoxide.sh
@@ -1,8 +1,16 @@
-# ----------------------------
-# Zodide Installation & Config
-# ----------------------------
-sudo pacman -S --noconfirm zoxide fzf
+#!/bin/bash
 
-tee -a ~/.zshrc <<'EOF'
-eval "$(zoxide init zsh)"
-EOF
+echo "=== Install and configure Kitty ==="
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+source "$SCRIPT_DIR/../helper_functions.sh"
+
+install_if_not_found zoxide
+install_if_not_found fzf
+
+if [[ -f ~/.zshrc ]]; then
+  if ! grep -q 'eval "$(zoxide init zsh)"' ~/.zshrc; then
+    echo "Adding zoxide to zshrc..."
+    echo -e "\n# zoxide\neval \"\$(zoxide init zsh)\"" >>~/.zshrc
+  fi
+fi
